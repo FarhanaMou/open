366cf2d0d02c3daf277688c3300035b0709109ec
FarhanaMou <farhanamou1010@gmail.com>
2020-07-21 11:25:35 +0600

    first commit

---
+++ object.js
@@ -0,0 +1,16 @@
+
+// var  productName = "computer";
+// var  productPrice = 5000;
+// var  quentity = 3;
+// var   stock = true;
+
+var productInfo ={
+    productName : "computer",
+    productPrice : 5000,
+    quentity : 3,
+    stock : true
+
+}
+// function 
+
+// console.log (`productName = ${productInfo.productName }   productPrice =    ${productInfo.productPrice} , quentity = ${productInfo. quentity }  , stock = ${productInfo.stock}`);
